550480268829b56265ad5669209e9769463d619a
maty lo <maty.lo@etu.u-paris.fr>
2025-12-29 20:39:25 +0000

    nouveau modele1

--- backend/ml/gzip.py
+++
@@ -1,6 +0,0 @@
-import pickletools
-
-with open("backend/ml/wildfire_spread_classifier_advanced.joblib", "rb") as f:
-    data = f.read()
-
-print(pickletools.dis(data))
--- backend/ml/test.py
+++
@@ -1,3 +0,0 @@
-import joblib
-model = joblib.load('backend/ml/wildfire_spread_classifier_advanced.joblib')
-print(model.__class__.__module__)
